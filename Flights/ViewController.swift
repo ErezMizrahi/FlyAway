3b0313529f77524e2ba76d770fbacb43911ea366
hackeru <hackeru@hackerus-Mac.local>
2019-07-15 09:59:22 -0700

    Initial Commit

---
+++ Flights/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flights
+//
+//  Created by hackeru on 15/07/2019.
+//  Copyright © 2019 erez8. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
